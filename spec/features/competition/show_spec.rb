9cf9c533425ae5a1d22ce23a59e25f86417259ca
Elliot Olbright <elliotolbright@gmail.com>
2021-08-04 09:58:42 -0600

    set up test for second part of user stroy 2

--- spec/features/competition/show_spec.rb
+++ spec/features/competition/show_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe 'Competition Index Page' do
   before :each do 
     @comp1 = Competition.create!(name: 'World Cup', location: 'New York', sport: 'Baseball')
 
+    @team1 = Team.create!(hometown: 'Denver', nickname: 'Rockies', competition_id: @comp1.id)
+    @team2 = Team.create!(hometown: 'San Francisco', nickname: 'Giants', competition_id: @comp1.id)
+
     visit competition_path(@comp1.id)
   end 
 
@@ -11,5 +14,13 @@ RSpec.describe 'Competition Index Page' do
     expect(page).to have_content(@comp1.name)
     expect(page).to have_content(@comp1.location)
     expect(page).to have_content(@comp1.sport)
+  end 
+
+  it ' can display team names' do 
+    expect(page).to have_content('Teams in this competition')
+    expect(page).to have_content(@team1.hometown)
+    expect(page).to have_content(@team1.nickname)
+    expect(page).to have_content(@team2.hometown)
+    expect(page).to have_content(@team2.nickname)
   end 
 end 
